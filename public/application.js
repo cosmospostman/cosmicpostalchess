a9de23d5e9dbd6403df2ac57be7bdd3a12217137
Mitchell Jeffrey <mitchell.lee.jeffrey@gmail.com>
2012-05-20 20:32:28 -0700

    Create 64 squares with js instead of explicitly defining each of them in HTML

--- public/application.js
+++ public/application.js
@@ -171,3 +171,14 @@ function newGame(gameID){
 //  $('#askName').hide();
 //  newGame('');
 
+// ### Post-refactored code below this line
+$(function(){
+  //Create invisible squares
+  //<div id="a8" class="square ui-droppable" >a8</div>
+  var alphaCoords = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  for (var i=8; i>0; i--)
+    for (var j=0; j<8; j++)
+      $('#board').append('<div class="square ui-droppable" id="' + alphaCoords[j<D-r>] + i + '"></div>');
+
+});
+
