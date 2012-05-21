695ac6784514f334202b869bf240dd86fc682228
Mitchell Jeffrey <mitchell.lee.jeffrey@gmail.com>
2012-05-20 20:49:58 -0700

    Move clock logic to js/clock.js

--- public/game.js
+++ public/game.js
@@ -144,35 +144,6 @@ function setGameVars(fayeClient, cfg){
 
 }
 
-//Pass a string here
-function  clockTick(colour){
-  //Init
-  this.element=colour+"Timer";
-  if( $("#"+element).data('seconds')==undefined) $("#"+element).data('seconds', 0);
-  if(whoseMove==colour){
-    window.setTimeout("clockTick('"+colour+"')", 1000);
-    s=$('#'+element).data('seconds');
-    m=0;
-    $('#'+element).data('seconds',s+1); //Increment
-    while(s>59){
-      s=s-60;
-      m++;
-    }
-    this.sDisplay=s;
-    if (s<10) this.sDisplay="0"+s;
-    $("#"+element+">span.clock").text(m+":"+this.sDisplay);
-  }
-}
-
-function  clockSwap(){
-  lastMove=whoseMove;
-    $("#"+whoseMove+"Timer").removeClass("active");
-    if(whoseMove=="white") whoseMove="black";
-    else if(whoseMove=="black") whoseMove="white";
-    $("#"+whoseMove+"Timer").addClass("active");
-    window.setTimeout("clockTick('"+whoseMove+"')", 1000);
-}
-
 function send(move){
   client.publish(p2pComms, {from: cfg.myColour, move:move });
 }
